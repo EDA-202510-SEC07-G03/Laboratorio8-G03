fdbde6bd6d5e2548ab7759ab7d911d0e3709a438
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-04-03 14:51:39 -0500

    commit

---
+++ DataStructures/Tree/binary_search_tree.py
@@ -0,0 +1,7 @@
+from DataStructures.Tree import bst_node as bst
+from DataStructures.Map import map_linear_probing as lp
+from DataStructures.List import array_list as ar
+def new_map():
+    tree = {"root": None}
+    return tree
+
