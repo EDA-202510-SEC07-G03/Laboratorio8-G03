e8359ad8020a3aaa922b61329ce698a054f517a5
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-04-08 22:57:12 -0500

    Avances arbol binario (falta dmin, dmax y is_empty)

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -49,3 +49,86 @@ def size_tree(root):
 def size(my_bst):
     return size_tree(my_bst["root"])
 
+def contains(my_bst, key):
+    result = get(my_bst, key)
+    if result is None:
+        return False
+    else:
+        return True  
+
+def key_set_tree(root):
+    list_retorno = sl.new_list()
+    if root == None:
+        return list_retorno
+    else:
+        sl.add_last(list_retorno, root["key"])
+        key_set_tree(root["left"])
+        key_set_tree(root["right"])
+    return list_retorno
+
+def key_set(my_bst):
+    return key_set_tree(my_bst["root"])
+
+def value_set_tree(root):
+    list_retorno = sl.new_list()
+    if root == None:
+        return list_retorno
+    else:
+        sl.add_last(list_retorno, root["value"])
+        value_set_tree(root["left"])
+        value_set_tree(root["right"])
+    return list_retorno
+
+def value_set(my_bst):
+    return value_set_tree(my_bst["root"])
+
+def get_min_node(root):
+    result = None
+    if root != None:
+        result = root["key"]
+        get_min_node(root["left"])
+    return result
+
+def get_min(my_bst):
+    return get_min_node(my_bst["root"])
+
+def get_max_node(root):
+    result = None
+    if root != None:
+        result = root["key"]
+        get_max_node(root["right"])
+    return result
+
+def get_max(my_bst):
+    return get_max_node(my_bst["root"])
+
+def keys_range(root, key_i, key_f, list_key):
+    if root == None:
+        return list_key
+    else:
+        if key_i <= root["key"] <= key_f:
+            sl.add_last(list_key, root["key"])
+        keys_range(root["left"], key_i, key_f, list_key)
+        keys_range(root["right"], key_i, key_f, list_key)
+    return list_key
+
+def keys(my_bst, key_i, key_f):
+    list_keys = sl.new_list()
+    return keys_range(my_bst["root"], key_i, key_f, list_keys)
+
+def values_range(root, value_i, value_f, list_value):
+    if root == None:
+        return list_value
+    else:
+        if value_i <= root["value"] <= value_f:
+            sl.add_last(list_value, root["value"])
+        values_range(root["left"], value_i, value_f, list_value)
+        values_range(root["right"], value_i, value_f, list_value)
+    return list_value
+
+def values(my_bst, value_i, value_f):
+    list_values = sl.new_list()
+    return values_range(my_bst["root"], value_i, value_f, list_values)
+
+        
+            
